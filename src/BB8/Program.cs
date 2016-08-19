6a120a72112b4cce98f69a8b955b77d6491613b5
Matt DeKrey <mattdekrey@gmail.com>
2016-08-19 07:37:06 -0500

    Motors are spinning!

--- src/BB8/Program.cs
+++ src/BB8/Program.cs
@@ -12,7 +12,7 @@ namespace BB8
     // GPIO Pins
     // 4 - 5V
     // 6 - Ground
-    // 7 - GPIO 4 - PWM motor 1 (orange)
+    // 7 - GPIO 4 - PWM motor 2 (orange)
     // 9 - Ground - DIR_EN (green)
     // 11 - GPIO 17 - Serial Data (yellow)
     // 13 - GPIO 27 - Serial Latch (blue)
@@ -27,52 +27,74 @@ namespace BB8
             // Thanks to http://blog.bennymichielsen.be/2016/03/14/getting-up-and-running-with-mono-and-raspberry-pi-3/
             var driver = GpioConnectionSettings.DefaultDriver;
 
-            var sensorPin = ((ProcessorPin)18).Input();
+            //var sensorPin = ((ProcessorPin)18).Input();
 
             var pwmOutput = ((ProcessorPin)4).Output();
             var serialDataPin = ((ProcessorPin)17).Output();
             var serialLatchPin = ((ProcessorPin)27).Output();
             var serialClockPin = ((ProcessorPin)22).Output();
 
-            var connection = new GpioConnection(new GpioConnectionSettings
-            {
-                PollInterval = TimeSpan.FromMilliseconds(0.001)
-            }, sensorPin);
+            //var connection = new GpioConnection(new GpioConnectionSettings
+            //{
+            //    PollInterval = TimeSpan.FromMilliseconds(0.001)
+            //}, sensorPin);
             var outConnection = new GpioConnection(pwmOutput, serialDataPin, serialLatchPin, serialClockPin);
-            connection.Open();
+            //connection.Open();
             outConnection.Open();
 
             var sw = new Stopwatch();
-            connection.PinStatusChanged += (sender, statusArgs) => 
-            {
-                if (statusArgs.Configuration.Pin == sensorPin.Pin)
-                {
-                    Console.Write(statusArgs.Enabled ? 1 : 0);
-                }
-            };
+            //connection.PinStatusChanged += (sender, statusArgs) => 
+            //{
+            //    if (statusArgs.Configuration.Pin == sensorPin.Pin)
+            //    {
+            //        Console.Write(statusArgs.Enabled ? 1 : 0);
+            //    }
+            //};
 
+            // bit 5 - motor header 1/2 pin 5
+            // bit 2 - motor header 1/2 pin 4
             sw.Start();
             try
             {
                 var serial = new SerialDigitizer(outConnection, data: serialDataPin, latch: serialLatchPin, clock: serialClockPin, bitCount: 8);
-                using (connection)
+                //using (connection)
                 using (outConnection)
-                //using (var pwm = new PwmThread(outConnection))
+                using (var pwm = new PwmThread(outConnection))
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
 
                     Console.WriteLine(DateTime.Now.ToString());
 
-                    //pwm.SetPhaseWidth(pwmOutput, 0.2);
-
-                    while (Console.ReadKey().Key != ConsoleKey.Enter)
+                    double pw = 1;
+                    pwm.SetPhaseWidth(pwmOutput, pw);
+                    ConsoleKey key;
+                    while ((key = Console.ReadKey().Key) != ConsoleKey.Enter)
                     {
-                        Console.WriteLine();
-                        serial.WriteData(0xff).Wait();
-                        Console.WriteLine();
+                        if (key == ConsoleKey.LeftArrow)
+                        {
+                            serial.WriteData(2).Wait();
+                        }
+                        else if (key == ConsoleKey.RightArrow)
+                        {
+                            serial.WriteData(0x10).Wait();
+                        }
+                        else if (key == ConsoleKey.DownArrow)
+                        {
+                            pwm.SetPhaseWidth(pwmOutput, pw -= 0.01);
+                        }
+                        else if (key == ConsoleKey.UpArrow)
+                        {
+                            pwm.SetPhaseWidth(pwmOutput, pw += 0.01);
+                        }
+                        Console.WriteLine(pw);
                     }
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Ending");
+
+                    serial.WriteData(0).Wait();
+                    // Give the Pi the time to process the signal so it actually shuts down
+                    Thread.Sleep(1000);
+
                 }
             }
             finally
